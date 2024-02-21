4b62e89c599f4bee00335d3ebd0e0e851fd049f5
Ana Júlia Pontes <ana-pontes@aluno.senai.br>
2024-02-21 16:57:42 -0300

    .

--- App.jsx
+++ App.jsx
@@ -1,10 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, TextInput, TouchableOpacity, View, StyleSheet, ScrollView } from 'react-native';
+import { useState } from "react";
 
 export default function App() {
+  const [task, setTask] = useState("");
+  const [list, setList] = useState([]);
+
+  const addTask = () => {
+    if (task.trim().length > 0) {
+      setList([...list, {
+        id: Math.random().toString(),
+        value: task
+      }]);
+      setTask("");
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.form}>
+        <TextInput
+          placeholder="Digite uma nova tarefa"
+          style={styles.input}
+          onChangeText={setTask}
+          value={task}
+        />
+        <TouchableOpacity onPress={addTask} style={styles.addButton}>
+          <Text>Adicionar</Text>
+        </TouchableOpacity>
+      </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {list.map((task) => (
+          <View
+            key={task.id}
+          >
+          <Text>{task.value}</Text>
+          </View>
+        ))}
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +44,24 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
   },
+  form: {
+    display: "flex",
+    flexDirection: "row",
+    justiyContent: "space-between",
+    alignItems: "center",
+    marginBottom: "20",
+  },
+  input: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "black",
+    width: "75%",
+  },
+  addButton: {
+    backgroundColor: "pink",
+    padding: 10,
+    borderRadius: 5,
+  }
 });
